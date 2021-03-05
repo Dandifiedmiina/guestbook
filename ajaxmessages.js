9b6f4ac7438923aab16a76e0bb1c1cfa78354f82
Dandifiedmiina <63451212+Dandifiedmiina@users.noreply.github.com>
2021-03-05 23:22:35 +0200

    Add files via upload

---
+++ ajaxmessages.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function () {
+
+   
+    $("#ajaxbutton").click(function () {
+             var data = {
+            name: $("#name2").val(),
+            country: $("#country2").val(),
+            message: $("#message2").val()
+        };
+     
+        if ($("#nameajax").val() == "") {
+            alert("Check that you have filled every field!");
+        } else if ($("#countryajax").val() == "") {
+            alert("Check that you have filled every field!");
+        } else if ($("#messageajax").val() == "") {
+            alert("Check that you have filled every field!");
+        } else {
+              
+        $.post("/ajaxmessage", data, function (response, status) {
+            $("#msgtableajax").html(response);
+        });
+        }
+    });
+
+});
--- index.js
+++ index.js
@@ -40,14 +40,14 @@ app.post("/newmessage2", function (req, res) {
 
   var datanew = JSON.stringify(json, "", 1);
 
-  fs.writeFileSync("./project1/guestbook.json", datanew);
+  fs.writeFileSync("./guestbook.json", datanew);
 
   res.send("Message was sent!");
   console.log("added");
 });
 
 app.get("/ajaxmessage", function (req, res) {
-  res.sendFile(__dirname + "/public/ajaxmessage.html");
+  res.sendFile(__dirname + "/ajaxmessage.html");
 });
 
 app.get("/readguestbook", function (req, res) {
@@ -59,7 +59,7 @@ app.get("*", function (req, res) {
 });
 
 // Web-palvelimen luonti
-app.listen(3003, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log("Example app listening on port 3003!");
 });
 
