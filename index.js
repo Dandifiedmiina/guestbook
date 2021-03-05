767b6b8163705b5f9b1ff799042009194e94011a
Dandifiedmiina <63451212+Dandifiedmiina@users.noreply.github.com>
2021-03-05 17:18:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+var http = require("http");
+var express = require("express");
+var app = express();
+var fs = require("fs");
+const path = require("path");
+
+// Otetaan body-parser -moduuli käyttöön
+var bodyParser = require("body-parser");
+const { json } = require("body-parser");
+
+// Otetaan se käyttöön app-nimisessä express-sovelluksessa
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("./"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "\\index.html");
+});
+
+app.get("/newmessage", function (req, res) {
+  res.sendFile(__dirname + "\\newmessage.html");
+});
+
+app.post("/newmessage2", function (req, res) {
+  var name = req.body.name;
+  var country = req.body.country;
+  var message = req.body.message;
+
+  var json = require("./guestbook.json");
+
+  console.log("Taulukon koko: " + json.length);
+
+  var newitem = {
+    name: name,
+    country: country,
+    message: message,
+  };
+
+  json.push(newitem);
+
+  var datanew = JSON.stringify(json, "", 1);
+
+  fs.writeFileSync("./project1/guestbook.json", datanew);
+
+  res.send("Message was sent!");
+  console.log("added");
+});
+
+app.get("/ajaxmessage", function (req, res) {
+  res.sendFile(__dirname + "/public/ajaxmessage.html");
+});
+
+app.get("/readguestbook", function (req, res) {
+  res.sendFile(__dirname + "\\readguestbook.html");
+});
+
+app.get("*", function (req, res) {
+  res.send("Cant find the requested page", 404);
+});
+
+// Web-palvelimen luonti
+app.listen(3003, function () {
+  console.log("Example app listening on port 3003!");
+});
+
+function results(err, data) {
+  if (err) return console.error(err);
+  console.log("log");
+}
---
+++ messages.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+ $.getJSON("guestbook.json", function (data) {
+  var table = "<table><tr><th>Names</th><th>Countries</th><th>Messages</th><th>Dates</th></tr>";
+  for (var x = 0; x < data.length; x++) {
+    table +=
+      "<tr>" +
+      "<td>" +
+      data[x].name +
+      "</td>" +
+      "<td>" +
+      data[x].country +
+      "</td>" +
+      "<td>" +
+      data[x].message +
+      "</td>" +
+      "<td>" ;
+  }
+
+  $("#msgtable").html(table);
+ });
+});
